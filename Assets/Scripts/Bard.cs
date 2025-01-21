0ebda1a74634c3ac2891cfe77d7499ab8166e7ef
Jeff Valentic <valen136@msu.edu>
2025-01-20 23:20:20 -0500

    bard constructors created

---
+++ Assets/Scripts/Bard.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using static Dictionary;
+using static DeckObj;
+
+public class Bard
+{
+    private DeckObj deck;
+    private Dictionary dict;
+    private int ego;
+
+    public Bard(Dictionary d, DeckObj new_deck, int e)
+    {
+        dict = d;
+        deck = new_deck;
+        ego = e;
+    }
+
+    public Bard(Dictionary d, DeckObj new_deck)
+    {
+        dict = d;
+        deck = new_deck;
+        ego = 100;
+    }
+
+    public Bard(Dictionary d)
+    {
+        dict = d;
+        deck = new DeckObj();
+        ego = 100;
+    }
+
+
+}
