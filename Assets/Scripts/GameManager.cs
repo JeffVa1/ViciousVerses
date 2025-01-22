0202b4433f5be5a82ab31ea39200a389e6782604
JeffVa1 <valen136@msu.edu>
2025-01-22 08:31:41 -0500

    Game manager card parsing (#8)
    * adding necessary fields to the card class
    
    * preparing to load cards from json
    
    * Moved json to a Data directory
    
    * final touches before reading card data into objects
    
    * Notice for broken TestScript

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,9 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+
 using static Bard;
 using static Dictionary;
 using static Card;
@@ -12,7 +17,16 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Read card json and add starter cards to (Player)Bards dictionary.
+        Debug.Log("LOADING NOUNS");
+        string noun_filename = "defaultNouns.json";
+        List<Card> nouns = ParseCardsFromJson(noun_filename);
+        LogCardArray(nouns);
+
+        Debug.Log("LOADING VERBS");
+        string verb_filename = "defaultVerbs.json";
+        List<Card> verbs = ParseCardsFromJson(verb_filename);
+        LogCardArray(verbs);
+
         // Read journal json and add phrases to (Player)Bards journal.
         // Set (Player)Bards starter deck.
 
@@ -21,6 +35,44 @@ public class GameManager : MonoBehaviour
         
 
         // Display welcome message.
+    }
+
+    // Method to parse the JSON string into an array of Card objects
+    private List<Card> ParseCardsFromJson(string filename)
+    {
+        string jsonFilePath = Path.Combine(Application.dataPath, "Data", filename);
+
+        // Read the JSON file content
+        string json = File.ReadAllText(jsonFilePath);
+
+        var cardDict = JsonConvert.DeserializeObject<Dictionary<string, List<CardData>>>(json);
+        List<Card> cards = new List<Card>();
+
+        foreach (var entry in cardDict)
+        {
+            foreach (var cardData in entry.Value)
+            {
+                Card card = new Card(
+                    text: entry.Key,
+                    multiplier: (int)cardData.ptMultiplier,
+                    addition: cardData.ptValue,
+                    pos: "N/A", // Placeholder, adjust if necessary
+                    e: cardData.egoDmg,
+                    audience: cardData.audienceValue
+                );
+                cards.Add(card);
+            }
+        }
+
+        return cards;
+    }
+
+    void LogCardArray(List<Card> cards)
+    {
+        foreach (var card in cards)
+        {
+            Debug.Log("Loaded Card: " + card.GetText());
+        }
     }
 
     void StartMatch() 
@@ -45,4 +97,13 @@ public class GameManager : MonoBehaviour
     }
 
 
+}
+
+// Helper class for deserialization
+public class CardData
+{
+    public int ptValue { get; set; }
+    public float ptMultiplier { get; set; }
+    public int egoDmg { get; set; }
+    public int audienceValue { get; set; }
 }
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -6,48 +6,50 @@ using System.Collections.Generic;
 
 public class TestScript : MonoBehaviour
 {
+    // BROKEN - added more fields to the Card object so these tests will just error. Didnt have time to fix
+    // TODO - fix Card instantiation in this script.
     void Start()
     {
-        Dictionary testDict = new Dictionary();
-        Card c1 = new Card("cTest1", 1, 10);
-        c1.LogCard(true);
-        Card c2 = new Card("bTest2", 2, 20);
-        Card c3 =  new Card("aTest3", 3, 30);
-        Card c4 = new Card("aTest4", 4, 40);
-        Card c5 = new Card("aTest5", 5, 50);
-        testDict.AddCard(c1);
-        testDict.AddCard(c2);
-        testDict.AddCard(c3);
-
-        testDict.LogCards(true);
-        testDict.Alphabetize();
-        testDict.LogCards(true);
-
-        List<Card> newDeck1 = new List<Card> { c1, c2 };
-        List<Card> newDeck2 = new List<Card> { c1, c2, c3, c4, c5};
-        DeckObj deckObj1 = new DeckObj(newDeck1);
-        DeckObj deckObj2 = new DeckObj();
-        Debug.Log("constructor 1 test - library1 size: " + deckObj1.GetLibraryCount());
-        deckObj2.SetLibrary(newDeck2);
-        Debug.Log("constructor 2 test - library2 size: " + deckObj2.GetLibraryCount());
-
-        Debug.Log("Add to library test - library1 size: " + deckObj1.GetLibraryCount());
-        deckObj1.AddCardToLibrary(c3);
-        Debug.Log("Add to library test - library1 size: " + deckObj1.GetLibraryCount());
-
-        Debug.Log("discard test post call - library1 size: " + deckObj1.GetLibraryCount());
-        deckObj1.RemoveCardFromLibrary(c3);
-        Debug.Log("discard test post call - library1 size: " + deckObj1.GetLibraryCount());
-
-        Debug.Log("draw player hand pre - library2 size: " + deckObj2.GetLibraryCount());
-        Debug.Log("draw player hand pre - playerHand size: " + deckObj2.GetHandCount());
-        Debug.Log("draw player hand pre - graveyard size: " + deckObj2.GetGraveyardCount());
-
-        deckObj2.DrawMaxPlayerHandFromLibrary(newDeck1);
-
-        Debug.Log("draw player hand post - library2 size: " + deckObj2.GetLibraryCount());
-        Debug.Log("draw player hand post- playerHand size: " + deckObj2.GetHandCount());
-        Debug.Log("draw player hand post - graveyard size: " + deckObj2.GetGraveyardCount());
+        //Dictionary testDict = new Dictionary();
+        //Card c1 = new Card("cTest1", 1, 10);
+        //c1.LogCard(true);
+        //Card c2 = new Card("bTest2", 2, 20);
+        //Card c3 =  new Card("aTest3", 3, 30);
+        //Card c4 = new Card("aTest4", 4, 40);
+        //Card c5 = new Card("aTest5", 5, 50);
+        //testDict.AddCard(c1);
+        //testDict.AddCard(c2);
+        //testDict.AddCard(c3);
+//
+        //testDict.LogCards(true);
+        //testDict.Alphabetize();
+        //testDict.LogCards(true);
+//
+        //List<Card> newDeck1 = new List<Card> { c1, c2 };
+        //List<Card> newDeck2 = new List<Card> { c1, c2, c3, c4, c5};
+        //DeckObj deckObj1 = new DeckObj(newDeck1);
+        //DeckObj deckObj2 = new DeckObj();
+        //Debug.Log("constructor 1 test - library1 size: " + deckObj1.GetLibraryCount());
+        //deckObj2.SetLibrary(newDeck2);
+        //Debug.Log("constructor 2 test - library2 size: " + deckObj2.GetLibraryCount());
+//
+        //Debug.Log("Add to library test - library1 size: " + deckObj1.GetLibraryCount());
+        //deckObj1.AddCardToLibrary(c3);
+        //Debug.Log("Add to library test - library1 size: " + deckObj1.GetLibraryCount());
+//
+        //Debug.Log("discard test post call - library1 size: " + deckObj1.GetLibraryCount());
+        //deckObj1.RemoveCardFromLibrary(c3);
+        //Debug.Log("discard test post call - library1 size: " + deckObj1.GetLibraryCount());
+//
+        //Debug.Log("draw player hand pre - library2 size: " + deckObj2.GetLibraryCount());
+        //Debug.Log("draw player hand pre - playerHand size: " + deckObj2.GetHandCount());
+        //Debug.Log("draw player hand pre - graveyard size: " + deckObj2.GetGraveyardCount());
+//
+        //deckObj2.DrawMaxPlayerHandFromLibrary(newDeck1);
+//
+        //Debug.Log("draw player hand post - library2 size: " + deckObj2.GetLibraryCount());
+        //Debug.Log("draw player hand post- playerHand size: " + deckObj2.GetHandCount());
+        //Debug.Log("draw player hand post - graveyard size: " + deckObj2.GetGraveyardCount());
     }
 
     void Update()
