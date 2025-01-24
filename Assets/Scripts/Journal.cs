aff8a3826d7fb106240b5b30dd8cdc52713101e6
Jeff Valentic <valen136@msu.edu>
2025-01-23 22:49:09 -0500

    Journal helper methods created

--- Assets/Scripts/Journal.cs
+++ Assets/Scripts/Journal.cs
@@ -9,7 +9,6 @@ public class Journal
     private List<JournalPhrase> usedPhrases;
     private JournalPhrase currentPhrase;
 
-
     public Journal(List<JournalPhrase> phrases)
     {
         availablePhrases = phrases;
@@ -36,6 +35,56 @@ public class Journal
         Debug.Log(currentPhrase.GetPhraseText());
         Debug.Log("Num words: " + currentPhrase.GetPhraseList().Count);
         Debug.Log("Num blank: " + currentPhrase.GetNumBlanks());
+    }
+
+    public void AddNewPhrase(JournalPhrase new_phrase)
+    {
+        availablePhrases.Add(new_phrase);
+    }
+
+    public bool SelectNewPhrase()
+    {
+        if (availablePhrases.Count > 0)
+        {
+            int randomIndex = Random.Range(0, availablePhrases.Count);
+            JournalPhrase random_phrase = availablePhrases[randomIndex];
+            availablePhrases.RemoveAt(randomIndex);
+            UpdateCurrentPhrase(random_phrase);
+        }
+        else
+        {
+            Debug.Log("ERROR - no more phrases available in journal");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void UpdateCurrentPhrase(JournalPhrase new_phrase)
+    {
+        if (currentPhrase == null)
+        {
+            currentPhrase = new_phrase;
+        } 
+        else 
+        {
+            usedPhrases.Add(currentPhrase);
+            currentPhrase = new_phrase;
+        }
+    }
+
+    public void ResetPhrases()
+    {
+        foreach (JournalPhrase p in usedPhrases)
+        {
+            availablePhrases.Add(p);
+        }
+
+        if (currentPhrase != null)
+        {
+            availablePhrases.Add(currentPhrase);
+            currentPhrase = null;
+        }
     }
 
 
