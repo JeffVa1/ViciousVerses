2f120d13eea30fb768d54a35e110ebfd865fa779
David Swanstrom <51976389+ifritborn@users.noreply.github.com>
2025-01-29 19:28:40 -0700

    Revert "Transitions game"

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,23 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MainMenu: MonoBehaviour
 {
-    public LevelLoader levelLoader;
-
     public void PlayGame()
     {
-        if (levelLoader != null)
-        {
-            levelLoader.CauseSceneTransition();
-        }
-        else
-        {
-            Debug.LogWarning("LevelLoader is not assigned! Loading scene without transition.");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
     public void QuitGame()
     {
         Debug.Log("QUIT!");
