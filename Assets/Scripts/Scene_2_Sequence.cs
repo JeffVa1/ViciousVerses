7327cfedd58146ee1097c653bb7cf09c5ea56a83
Ifritborn <ifritborndesigns@gmail.com>
2025-01-31 00:07:14 -0700

    scene2

--- Assets/Scripts/Scene_2_Sequence.cs
+++ Assets/Scripts/Scene_2_Sequence.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 public class Scene_2_Sequence : MonoBehaviour
 {
- 
-    public SpriteRenderer BarRenderer;     
-    // public SpriteRenderer PlayerRenderer;  
-    // public SpriteRenderer BarmaidRenderer;     
-    // public SpriteRenderer Enemy2Renderer;
+
+    public SpriteRenderer BarRenderer;
+    public SpriteRenderer PlayerRenderer;
+    public SpriteRenderer BarmaidRenderer;
+    public SpriteRenderer Enemy1Renderer;
+    public SpriteRenderer Enemy2Renderer;
     public float FadeDuration = 1f; // Duration for fade effects
 
     public DialogueManager DialogueManager;
@@ -22,16 +23,47 @@ public class Scene_2_Sequence : MonoBehaviour
         StartCoroutine(EventSequence());
     }
 
-    private IEnumerator EventSequence(){
-        
+    private IEnumerator EventSequence()
+    {
+
         Debug.Log("Scene 2 loaded sucessfully");
         yield return new WaitForSeconds(1);
         StartCoroutine(TheSpriteManager.SpriteFadeIn(BarRenderer));
 
-        // Debug.Log("Calling StartNextBattle");
-        // GameManager.Instance.StartNextBattle();
+        JsonLoader.LoadJson("scene2.json", (jsonData) =>
+        {
+            if (jsonData != null)
+            {
+                StartCoroutine(TheSpriteManager.DialogueFade(DialogueCanvasGroup, 0f, 1f, FadeDuration));
+                DialogueManager.StartDialogueWithJson(jsonData);
+            }
+            else
+            {
+                Debug.LogError("DialogueManager initialization failed due to missing or empty JSON Using UWR");
+                DialogueManager.Initialize("Assets/Data/openingScene.json");
+                StartCoroutine(TheSpriteManager.DialogueFade(DialogueCanvasGroup, 0f, 1f, FadeDuration));
+                DialogueManager.StartDialogue();
+            }
+        });
+
+        yield return StartCoroutine(TheSpriteManager.WaitForFlag(1));
+        StartCoroutine(TheSpriteManager.SpriteFadeIn(PlayerRenderer));
+        StartCoroutine(TheSpriteManager.SpriteFadeIn(Enemy1Renderer));
+
+        yield return StartCoroutine(TheSpriteManager.WaitForFlag(2));
+        StartCoroutine(TheSpriteManager.SpriteFadeOut(Enemy1Renderer));
+        StartCoroutine(TheSpriteManager.SpriteFadeIn(BarmaidRenderer));
+
+        yield return StartCoroutine(TheSpriteManager.WaitForFlag(3));
+        StartCoroutine(TheSpriteManager.SpriteFadeOut(BarmaidRenderer));
+        StartCoroutine(TheSpriteManager.SpriteFadeIn(Enemy2Renderer));
+
+        yield return StartCoroutine(TheSpriteManager.WaitForFlag(4));
+
+        Debug.Log("Calling StartNextBattle");
+        GameManager.Instance.StartNextBattle();
     }
 
 
-    
+
 }
