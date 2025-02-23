7343028bb89d8060c2cc725fbf3c4a9e5a2bb9d2
Ronald Martin <ronaldmartin02@gmail.com>
2025-02-22 19:14:38 -0500

    added quit button and added font to game over screen

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class GameOverManager : MonoBehaviour
@@ -5,5 +6,11 @@ public class GameOverManager : MonoBehaviour
     public void RestartGame()
     {
         GameManager.Instance.GoToNextScene();
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT! ");
+        Application.Quit();
+        
     }
 }
